effb61d0b47b85cb5008d88c522ce8342c541483
Brandon Murdoch <brandon.murdoch@argos.co.uk>
2021-04-02 17:17:10 +0100

    Init

---
+++ src/main/java/Roo/Demo.java
@@ -0,0 +1,11 @@
+package Roo;
+
+import java.util.Arrays;
+
+public class Demo {
+
+  public static void main(String[] args) {
+    System.out.println("Reading args: " + Arrays.toString(args));
+  }
+
+}
